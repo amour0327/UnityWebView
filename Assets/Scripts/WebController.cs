cf912dbfb1acf2474668a994a721f3f9b9cf7f18
naoki.saito <naoki.saito@enish.jp>
2014-02-03 21:24:29 +0900

    WebViewを表示する

---
+++ Assets/Scripts/WebController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebController : MonoBehaviour {
+
+	private string url = "http://terraform.sakura.ne.jp/blog/";
+	WebViewObject webViewObject;
+	void Start() {
+		webViewObject = GetComponent<WebViewObject>();
+		webViewObject.Init();
+		webViewObject.LoadURL(url);
+		webViewObject.SetVisibility(true);
+	}
+}
